7ac5075627a9e59dd98755e34ad0f9022f7b60d9
Henrik Djärv <henrik@djarv.org>
2022-12-01 14:09:27 +0100

    Create repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+
+My contributions to the [Advent of Code](https://adventofcode.com/) for 2022.
---
+++ src/lib/utils.ts
@@ -0,0 +1,36 @@
+import { existsSync as fileExists, readFile as _readFile } from "fs";
+import { EOL } from "os";
+import { basename, extname, join } from "path";
+import { promisify } from "util";
+
+export { basename, EOL, extname, fileExists, join };
+
+export const readFile = promisify(_readFile);
+
+export const numToStr = (n: number): string => `${n}`;
+export const strToNum = (s: string): number => Number.parseInt(s, 10);
+
+export const findMinMaxInRange = (
+  numbers: number[],
+  start: number = 0,
+  end: number = numbers.length
+): { min: number; max: number } => {
+  let min = Number.MAX_VALUE,
+    max = Number.MIN_VALUE;
+  for (let i = start; i < end; i++) {
+    if (numbers[i] < min) min = numbers[i];
+    if (numbers[i] > max) max = numbers[i];
+  }
+  return { min, max };
+};
+export const sumRange = (numbers: number[], start: number = 0, end: number = numbers.length): number => {
+  let result = 0;
+  for (let i = start; i < end; i++) {
+    result += numbers[i];
+  }
+
+  return result;
+};
+
+export const sortAscending = (a: number, b: number): number => a - b;
+export const sortDescending = (a: number, b: number): number => b - a;
