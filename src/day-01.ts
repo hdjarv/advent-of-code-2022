e97b5708f216a3abfb513e48a3f2d5d68f957afb
Henrik Djärv <henrik@djarv.org>
2022-12-01 14:15:31 +0100

    Add solution for Day 1

---
+++ src/day-01.ts
@@ -0,0 +1,30 @@
+import { EOL, findMinMaxInRange, sortDescending, strToNum, sumRange } from "./lib/utils";
+
+function solvePart1(data: number[]): number {
+  return findMinMaxInRange(data).max;
+}
+
+function solvePart2(data: number[]): number {
+  return sumRange(data.sort(sortDescending), 0, 3);
+}
+
+export default async (input: string) => {
+  const data = input
+    .split(EOL)
+    .map(strToNum)
+    .reduce(
+      (result: number[][], value: number) => {
+        if (Number.isNaN(value)) {
+          result.push([]);
+        } else {
+          result[result.length - 1].push(value);
+        }
+        return result;
+      },
+      [[]]
+    )
+    .map((elfdata: number[]) => sumRange(elfdata));
+
+  console.log(`Part 1: The result is: ${solvePart1(data)}`); // 74394 is correct
+  console.log(`Part 2: The result is: ${solvePart2(data)}`); // 212836 is correct
+};
